b92e3c4fb4747e01f54de89cd6284f568c3b48bc
Ahortu901 <aricktech.edu@gmail.com>
2023-07-31 22:38:01 +0000

    input&output

---
+++ c++/i&o.cpp
@@ -0,0 +1,19 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    int a,b,c;
+    int reslut;
+    cout << "Please Enter three integer: ";
+    cin >> a >> b >> c;
+    cout << endl;
+    reslut = a + b + c ;
+    cout << reslut << endl;
+    
+    return 0;
+}
