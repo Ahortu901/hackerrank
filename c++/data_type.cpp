ca4f775ced197698ac5497e60c523cbeaf92e3bc
Ahortu901 <aricktech.edu@gmail.com>
2023-07-31 23:01:18 +0000

    updating data_types code

---
+++ c++/data_type.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    // Complete the code.
+    int ini;
+    long lo;
+    char ch;
+    float fl;
+    double dou;
+    
+    scanf("%d %ld %c %f %lf", &ini,&lo, &ch, &fl, &dou);
+    
+    printf("%d \n%ld \n%c \n%f \n%lf", ini, lo, ch, fl, dou);
+    return 0;
+}
